86992ef2dbe21ff48304b323857a3ec7f93005bf
rirze <cheedella.sourabh@gmail.com>
2016-07-15 22:36:02 -0400

    removed numba, too much overhead

--- gravpy/python/sie/sienumba.py
+++
@@ -1,6 +0,0 @@
-import numba
-import gravpy.python.sie.siepy as siepy
-
-spherical = numba.jit("f8[:,:](f8[:],f8[:],f8[:])")(siepy.spherical)
-
-elliptical = numba.jit("f8[:,:](f8[:],f8[:],f8[:])")(siepy.elliptical)
